a9f7f40d1ded08a349c71e6e91f01cbe7f0dc710
James Ide <ide@jameside.com>
2015-06-14 02:29:16 -0700

    [Tests] Fix basic acquireAsync test (add await), add test with co+gen

--- src/__tests__/AwaitLock-test.js
+++ src/__tests__/AwaitLock-test.js
@@ -2,15 +2,23 @@ jest.autoMockOff();
 
 require('regenerator/runtime');
 
+let co = require('co');
+
 let AwaitLock = require('../AwaitLock');
 
 describe('AwaitLock', () => {
 
   pit('can be acquired asynchronously and released', async function() {
     let lock = new AwaitLock();
-    lock.acquireAsync();
+    await lock.acquireAsync();
     lock.release();
   });
+
+  pit('can be acquired with yield running in co', co.wrap(function*() {
+    let lock = new AwaitLock();
+    yield lock.acquireAsync();
+    lock.release();
+  }));
 
   it('throws if released while unacquired', function() {
     let lock = new AwaitLock();
